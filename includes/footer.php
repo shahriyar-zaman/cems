b2cd3737cfe8fa552f61f1dd6ddfef6394a7bc37
thetonmoy <koushik.tonmoy@northsouth.edu>
2024-08-10 21:03:16 +0600

    footer updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer class="bg-dark text-white text-center py-3">
-    <p>This project was developed as part of CSE327.6 (Spring 2024)</p>
+    <!--<p>This project was developed as part of CSE327.6 (Spring 2024)</p>-->
     <p>
         <a href="mailto:shahriyar.ridoy@northsouth.edu" class="text-white">Shahriyar Zaman Ridoy 202229964</a><br>
         <a href="mailto:koushik.tonmoy@northsouth.edu" class="text-white">Koushik Ahamed Tonmoy 2111023642</a>
